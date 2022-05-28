d078cec247b8891875f0d8f58c0504372810008e
VanessaLiaw021 <vliaw021@gmail.com>
2022-05-28 12:02:11 -0400

    created a function that determine the hour of past, present, future

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,38 @@ $(document).ready(function() {
 
     //Call the function current Date
     currentDate();
+
+    //Function that display the correct color of textarea based on the past, present, and future hours
+    function determineHour() {
+
+        //Get the current hour of the day 
+        var currentHour = moment().hour();
+
+        //For each time-block, match the dataset attribute to the current hour to display the correct background color
+        $(".time-block").each(function() {
+
+            //Use the dataset attribute in html and get the number of each timeblock from 9 - 17
+            var dataNumberAttribute = parseInt($(this).attr("data-number"));
+
+            //Determine what is the current hour with get data number
+            if (dataNumberAttribute < currentHour) {
+
+                //Add the class past to display the hours that has past
+                $(this).addClass("past");
+
+            } else if (dataNumberAttribute === currentHour) {
+
+                //Add the class present to display the current hour that is now
+                $(this).addClass("present");
+
+            } else {
+
+                //Add the class future to display the future hour coming up
+                $(this).addClass("future");
+            }
+        });
+    }
+
+    //Call the function deteremine hour
+    determineHour();
 });
